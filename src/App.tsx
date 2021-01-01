60b34ed65942da798f4d13c151d7d017a86f0ea3
tgbf2674 <tgbf2020@163.com>
2021-01-01 12:44:34 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,22 @@ import {
     HashRouter as Router,
     Switch,
     Route,
-    Link,
     Redirect,
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
-  border: 1px solid red;
   height: 100vh;
   display: flex;
   flex-direction: column;
+
 `;
 
 const Main = styled.div`
-border: 1px solid green;
 flex-grow: 1;
 `;
-const Nav = styled.nav`
-border: 1px solid red;
-> ul{
-      display: flex;
-   > li {
-    width: 33.3333%;
-    text-align: center;
-    padding: 16px;
-    
-    }
-}
-`;
+
 
 function App() {
     return (
@@ -53,20 +41,7 @@ function App() {
                         </Route>
                     </Switch>
                 </Main>
-                <Nav>
-                    <ul>
-                        <li>
-                            <Link to="/money">记账页</Link>
-                        </li>
-                        <li>
-                            <Link to="/tags">标签页</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">统计页</Link>
-                        </li>
-                    </ul>
-                </Nav>
-
+                <Nav />
             </Wrapper>
         </Router>
     );
