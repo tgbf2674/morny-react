bd60398835632668cc04eee625610989842c3684
tgbf2674 <tgbf2020@163.com>
2021-01-04 20:40:17 +0800

    编辑tag 名

--- src/Tag.tsx
+++ src/Tag.tsx
@@ -1,5 +1,5 @@
 import {useTags} from 'useTags';
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import Layout from 'components/Layout';
 import React from 'react';
 import Icon from 'components/Icon';
@@ -25,28 +25,33 @@ background: white;
 padding: 0 16px;
 margin-top: 8px;
 `;
-const Tag: React.FC =(props)=>{
-    const {findTag} = useTags();
-    let {id} = useParams<Params>();
-        const tag = findTag(parseInt(id));
+
+const Tag: React.FC = (props) => {
+    const {findTag, updateTag} = useTags();
+    let {id:idString} = useParams<Params>();
+    const tag = findTag(parseInt(idString));
     return (
         <Layout>
-        <Topbar>
-            <Icon name="左" />
-            <span>编辑标签</span>
-            <Icon />
-        </Topbar>
+            <Topbar>
+                <Icon name="左"/>
+                <span>编辑标签</span>
+                <Icon/>
+            </Topbar>
             <InputWrapper>
-                <Input label="标签名" placeholder="标签名"  type="text" value={tag.name} />
-            </InputWrapper>
-            <Center>
-                <Space/>
-                <Space/>
-                <Space/>
-                <Button>删除标签</Button>
-            </Center>
-                </Layout>
-    )
-};
+                <Input label="标签名" placeholder="标签名" type="text"
+                       value={tag.name}
+                       onChange={(e) => {
+                           updateTag(tag.id, {name: e.target.value});
+                       }}/>
+                           </InputWrapper>
+                           <Center>
+                           <Space/>
+                           <Space/>
+                           <Space/>
+                           <Button>删除标签</Button>
+                           </Center>
+                           </Layout>
+                           );
+                           };
 
-export {Tag}
+                           export {Tag};
--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,6 +1,5 @@
 import {useState} from 'react';
 import {createId} from 'lib/createId';
-import {compilation} from 'webpack';
 
 const defaultTags = [
     {id:createId(),name:'衣'},
@@ -11,10 +10,31 @@ const defaultTags = [
 const useTags=()=> {
     const [tags, setTags] = useState<{id:number;name:string }[]>(defaultTags);
     const findTag = (id:number)=>tags.filter(tag=>tag.id===id)[0];
+    const findTagIndex = (id:number)=> {
+            let result = -1;
+            for (let i = 0; i< tags.length;i++){
+                if(tags[i].id ===id){
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        };
+    const updateTag= (id:number,obj: {name:string })=>{
+        //获取你要改的tag 的下标
+        const index = findTagIndex(id);
+        //深拷贝 tags 得到clone
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        //把tagsClone 的第index 删掉，换成{id：id,name: obj.name}
+        tagsClone.splice(index,1,{id,name:obj.name});
+        setTags(tagsClone);
+    };
     return {
         tags,
         setTags,
         findTag,
+        updateTag,
+        findTagIndex
     }
 };
 
